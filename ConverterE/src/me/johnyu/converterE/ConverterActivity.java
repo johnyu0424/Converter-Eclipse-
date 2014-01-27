d3fbd0efbec8a03b9f5028012ba94dd279cb0946
John Yu <johnyu0424@gmail.com>
2014-01-26 20:42:49 -0700

    Edit text works, seek bar works, buttons work, calculations are correct.Future might need to hide the keyboard when EditText is off focus.

--- ConverterE/src/me/johnyu/converterE/ConverterActivity.java
+++ ConverterE/src/me/johnyu/converterE/ConverterActivity.java
@@ -2,15 +2,21 @@ package me.johnyu.converterE;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Service;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class ConverterActivity extends Activity implements OnSeekBarChangeListener, OnClickListener{
+public class ConverterActivity extends Activity implements OnSeekBarChangeListener, OnClickListener, OnEditorActionListener{
 	// Initialize cTemp to be 0 and fTemp to be 32
 	public double cTemp = getCelsius(32);
 	public double fTemp = getFahrenheit(0);
@@ -33,6 +39,10 @@ public class ConverterActivity extends Activity implements OnSeekBarChangeListen
 		celsiusBox.setText(String.format("%.2f", cTemp));
 		EditText fahrenheitBox = (EditText) findViewById(R.id.fahrenheitBox);
 		fahrenheitBox.setText(String.format("%.2f", fTemp));
+		
+		// Event handler for finishing editing text box
+		celsiusBox.setOnEditorActionListener(this);
+		fahrenheitBox.setOnEditorActionListener(this);
 	}
 
 	@Override
@@ -62,14 +72,6 @@ public class ConverterActivity extends Activity implements OnSeekBarChangeListen
 
 	@Override
 	public void onStartTrackingTouch(SeekBar seekBar) {
-//		fTemp = seekBar.getProgress() / 10.0;
-//		cTemp = getCelsius(fTemp);
-//		
-//		EditText celsiusBox = (EditText) findViewById(R.id.celsiusBox);
-//		celsiusBox.setText(String.format("%.2f", cTemp));
-//		
-//		EditText fahrenheitBox = (EditText) findViewById(R.id.fahrenheitBox);
-//		fahrenheitBox.setText(String.format("%.2f", fTemp));
 	}
 
 	@Override
@@ -132,11 +134,49 @@ public class ConverterActivity extends Activity implements OnSeekBarChangeListen
 		}
 	}
 	
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		boolean handled = false;
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+        	// Hide the keyboard after hitting "Done"
+        	InputMethodManager imm = (InputMethodManager) getSystemService(Service.INPUT_METHOD_SERVICE);
+        	imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        	
+        	// Calling code to change the label and update seekbar
+            updateLabel(v);
+            handled = true;
+        }
+        return handled;
+	}
 	
-	
-	
-	
-	
+	public void updateLabel(View view)
+	{
+		if(view.getId() == R.id.celsiusBox)
+		{
+			EditText celsiusBox = (EditText) findViewById(R.id.celsiusBox);
+			cTemp = Double.parseDouble(celsiusBox.getText().toString());
+			fTemp = getFahrenheit(cTemp);
+			
+			EditText fahrenheitBox = (EditText) findViewById(R.id.fahrenheitBox);
+			fahrenheitBox.setText(String.format("%.2f", fTemp));
+			
+			SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+			seekBar.setProgress((int)fTemp * 10);
+		}
+		
+		else if(view.getId() == R.id.fahrenheitBox)
+		{	
+			EditText fahrenheitBox = (EditText) findViewById(R.id.fahrenheitBox);
+			fTemp = Double.parseDouble(fahrenheitBox.getText().toString());
+			cTemp = getCelsius(fTemp);
+			
+			EditText celsiusBox = (EditText) findViewById(R.id.celsiusBox);
+			celsiusBox.setText(String.format("%.2f", cTemp));
+			
+			SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+			seekBar.setProgress((int)fTemp * 10);
+		}
+	}
 	
 	// Calculate corresponding Celsius value with given Fahrenheit value
 	public double getCelsius(double fahrenheit)
@@ -147,5 +187,5 @@ public class ConverterActivity extends Activity implements OnSeekBarChangeListen
 	public double getFahrenheit(double celsius)
 	{
 		return celsius * 9.0 / 5.0 + 32;
-	}
+	}	
 }
